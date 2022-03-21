3e8a6c7dca6181b60f46aa210a57812fa8ed35a1
szymekdr <geralttee@gmail.com>
2022-03-21 20:29:48 +1100

    Updates

--- code_brms.R
+++ code_brms.R
@@ -1,14 +1,15 @@
 # loading required packages ----
 library(pacman)
 p_load(brms, phytools, geiger, tidyverse, ape)
-
 library(here)
+# install.packages("cmdstanr", repos = c("https://mc-stan.org/r-packages/", getOption("repos")))
+# i_am("katana/katana_new_with_predictors/kat_220311_session1/code_brms.R")
 
 options(mc.cores = parallel::detectCores())
 
 # load data and tree ----
-data <- read_csv(here('data', 'hoarding_data_27Jul2020.csv'))
-tree <- read.tree(here('data', 'Stage2_Hackett_MCC_no_neg.tre'))
+data <- read_csv(here('../data/hoarding_data_27Jul2020.csv'))
+tree <- read.tree(here('../data/Stage2_Hackett_MCC_no_neg.tre'))
 tree_final <- drop.tip(tree, setdiff(tree$tip.label, data$tip_label))
 
 data_analysis <- filter(data, hoarding_status != 'unknown_hoarding_status')
@@ -23,15 +24,16 @@ A <- vcv.phylo(tree_final)
 
 
 model <- brmsformula(
-  hoarding_status ~ brain_size + log(body_weight) #+ (1|gr(tip_label, cov = A))
+  strat_final ~ 1 + (1|gr(tip_label, cov = A))
 )
 
 # get_prior(model, data = data_analysis, family = categorical, data2 = list(A = A))
 
-modelrun <- brm(model, chains = 2, iter = 2000, warmup = 1000,
+modelrun <- brm(model, chains = 4, iter = 12000, warmup = 4000, thin = 5,
                 data = data_analysis,
-                #data2 = list(A = A),
+                data2 = list(A = A),
                 family = categorical,
+                # backend = "cmdstanr",
                 control = list(adapt_delta = 0.9, max_treedepth = 15)
                 )
-summary(modelrun)
+
